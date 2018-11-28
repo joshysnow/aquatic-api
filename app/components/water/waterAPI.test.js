23aeaf9f24f1f5aeab170c243200497d1bc016aa
Joshua Hirst <hirst.joshua@me.com>
2018-11-28 12:39:47 +0000

    Tests for GET/id in water API

--- app/components/water/waterAPI.test.js
+++ app/components/water/waterAPI.test.js
@@ -7,8 +7,10 @@ const sinon = require('sinon');
 const chai = require('chai');
 
 describe('Water API', () => {
+  let app, router, request;
+
   describe('GET /water', () => {
-    let app, getWaterStub, router, request;
+    let getWaterStub;
 
     beforeEach(() => {
       getWaterStub = sinon.stub();
@@ -39,6 +41,45 @@ describe('Water API', () => {
 
       request
         .get('/')
+        .expect(200, (err, res) => {
+          chai.expect(res.body).to.deep.equal(testData);
+          done();
+        });
+    });
+  });
+
+  describe('GET /water/:id', () => {
+    let getWaterByIdStub;
+
+    beforeEach(() => {
+      getWaterByIdStub = sinon.stub();
+      app = express();
+      router = proxyquire('./waterAPI', {
+        './waterController': { // dependency of water API
+          getWaterTestById: getWaterByIdStub
+        }
+      });
+
+      app.use(router);
+
+      request = supertest(app);
+    });
+
+    it ('returns status 500 on error', (done) => {
+      getWaterByIdStub.yields(true, undefined);
+
+      request
+        .get('/1')
+        .expect(500, (err, res) => {
+          done();
+        });
+    });
+    it ('returns status 200 and data', (done) => {
+      const testData = [{test: 'test'}];
+      getWaterByIdStub.yields(false, testData);
+
+      request
+        .get('/1')
         .expect(200, (err, res) => {
           chai.expect(res.body).to.deep.equal(testData);
           done();
