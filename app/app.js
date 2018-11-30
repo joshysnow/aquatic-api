96033b1d925f693b9befeba77de0995907fd7d17
Joshua Hirst <hirst.joshua@me.com>
2018-11-30 14:27:11 +0000

    Activated CORS

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const dbConnection = require('./db');
 const app = express();
+
+app.use(cors());
 
 const Water = require('./components/Water');
 app.use('/water', Water.api);
